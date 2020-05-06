938c746e9ecc61b2deef5abc85a366948c5a9b45
tamagohan <fantastipo23@gmail.com>
2020-05-06 19:00:40 +0900

    Imp hello-world and high-scores

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,29 @@
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a [u32],
+}
+
+impl<'a> HighScores<'a> {
+    pub fn new(scores: &'a [u32]) -> Self {
+        Self { scores: scores }
+    }
+
+    pub fn scores(&self) -> &[u32] {
+        self.scores
+    }
+
+    pub fn latest(&self) -> Option<u32> {
+        self.scores.last().map(|x| *x)
+    }
+
+    pub fn personal_best(&self) -> Option<u32> {
+        self.scores.iter().max().map(|x| *x)
+    }
+
+    pub fn personal_top_three(&self) -> Vec<u32> {
+        let mut vec: Vec<u32> = self.scores.to_vec();
+        vec.sort_by(|a, b| (b.cmp(a)));
+        vec.truncate(3);
+        vec
+    }
+}
