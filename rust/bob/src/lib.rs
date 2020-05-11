e55f1a78253e5ca1dfefeac035f4347ea85d3d4e
tamagohan <fantastipo23@gmail.com>
2020-05-11 19:47:28 +0900

    [Refactoring] split into function

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,29 +1,18 @@
-use std::collections::HashSet;
-pub fn reply(message: &str) -> &str {
-    let spaces: HashSet<char> = " \t\r".chars().collect();
-    let uniq: HashSet<char> = message.chars().collect();
-    if uniq.is_subset(&spaces) {
-        return "Fine. Be that way!";
-    }
-
-    let is_question = message.trim_end_matches(" ").ends_with("?");
+fn is_yell(message: &str) -> bool {
     let is_uppercase_or_symbol = message.to_uppercase() == message;
     let is_contains_alphabetic = !message
         .matches(char::is_alphabetic)
         .collect::<Vec<&str>>()
         .is_empty();
 
-    if is_question {
-        if is_contains_alphabetic && is_uppercase_or_symbol {
-            return "Calm down, I know what I'm doing!";
-        } else {
-            return "Sure.";
-        }
+    is_uppercase_or_symbol && is_contains_alphabetic
+}
+pub fn reply(message: &str) -> &str {
+    match message.trim() {
+        m if m.len() == 0 => "Fine. Be that way!",
+        m if m.ends_with("?") && is_yell(m) => "Calm down, I know what I'm doing!",
+        m if m.ends_with("?") => "Sure.",
+        m if is_yell(m) => "Whoa, chill out!",
+        _ => "Whatever.",
     }
-
-    if is_contains_alphabetic && is_uppercase_or_symbol {
-        return "Whoa, chill out!";
-    }
-
-    return "Whatever.";
 }
