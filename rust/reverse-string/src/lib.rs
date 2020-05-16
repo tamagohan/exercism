84d23809f62af92d916400259bd468d591006b7e
tamagohan <fantastipo23@gmail.com>
2020-05-16 16:21:08 +0900

    [Refactoring] imp logic one line

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,7 +1,5 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let mut g = UnicodeSegmentation::graphemes(input, true).collect::<Vec<&str>>();
-    g.reverse();
-    g.into_iter().collect()
+    input.graphemes(true).rev().collect::<String>()
 }
