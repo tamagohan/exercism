274998b2d450692ec6ded3e4191424b472034795
tamagohan <fantastipo23@gmail.com>
2020-05-18 19:56:43 +0900

    Imp sum of multiples

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,7 +1,6 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    let factors_without_zero = factors.iter().filter(|x| *x != &0).collect::<Vec<&u32>>();
+    (1..limit)
+        .filter(|x| factors_without_zero.iter().any(|f| x % *f == 0))
+        .sum()
 }
