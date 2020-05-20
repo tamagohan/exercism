93c59a04d9ee0ff65aa3e2eba81e1d11a532e644
tamagohan <fantastipo23@gmail.com>
2020-05-20 20:49:36 +0900

    Imp grains

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,11 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    if s >= 1 && s <= 64 {
+        2_u64.pow(s - 1)
+    } else {
+        panic!("Square must be between 1 and 64")
+    }
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..65_u32).fold(0, |acc, x| acc + square(x))
 }
