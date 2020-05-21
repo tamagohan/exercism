e4a10132841cdfe01c6eb5f52322a9f9e2c720db
tamagohan <fantastipo23@gmail.com>
2020-05-21 20:00:37 +0900

    [Refactoring] use formula

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 pub fn square(s: u32) -> u64 {
     if s >= 1 && s <= 64 {
         2_u64.pow(s - 1)
@@ -7,5 +9,9 @@ pub fn square(s: u32) -> u64 {
 }
 
 pub fn total() -> u64 {
-    (1..65_u32).fold(0, |acc, x| acc + square(x))
+    let total_number_of_squares: u32 = 64;
+    // Use Geometric Series Sum Formula
+    (2_u128.pow(total_number_of_squares) - 1)
+        .try_into()
+        .unwrap()
 }
