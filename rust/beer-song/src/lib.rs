cdaead464229994eca73c6cc64e069d73f753fb0
tamagohan <fantastipo23@gmail.com>
2020-05-23 16:00:44 +0900

    [Refactoring] Declare const and use it in function

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,8 +1,12 @@
+const SONGS: [&str; 3] = [
+    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
+    "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
+    "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n",
+];
+
 pub fn verse(n: u32) -> String {
     match n {
-        0 => format!("{} bottles of beer on the wall, {} bottles of beer.\nGo to the store and buy some more, {} bottles of beer on the wall.\n", "No more", "no more", 99).to_string(),
-        1 => format!("{} bottle of beer on the wall, {} bottle of beer.\nTake it down and pass it around, {} bottles of beer on the wall.\n", 1, 1, "no more").to_string(),
-        2 => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottle of beer on the wall.\n", n, n, n - 1).to_string(),
+        n if n < 3 => SONGS[n as usize].to_string(),
         _ => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n - 1).to_string(),
     }
 }
@@ -13,5 +17,4 @@ pub fn sing(start: u32, end: u32) -> String {
         .rev()
         .collect::<Vec<String>>()
         .join("\n")
-        .to_string()
 }
