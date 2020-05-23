b386ec0d59dddcef2c1a9027f8b4fd8cb7649396
tamagohan <fantastipo23@gmail.com>
2020-05-23 13:20:30 +0900

    Imp beer song

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,7 +1,17 @@
 pub fn verse(n: u32) -> String {
-    unimplemented!("emit verse {}", n)
+    match n {
+        0 => format!("{} bottles of beer on the wall, {} bottles of beer.\nGo to the store and buy some more, {} bottles of beer on the wall.\n", "No more", "no more", 99).to_string(),
+        1 => format!("{} bottle of beer on the wall, {} bottle of beer.\nTake it down and pass it around, {} bottles of beer on the wall.\n", 1, 1, "no more").to_string(),
+        2 => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottle of beer on the wall.\n", n, n, n - 1).to_string(),
+        _ => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n - 1).to_string(),
+    }
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    unimplemented!("sing verses {} to {}, inclusive", start, end)
+    (end..(start + 1))
+        .map(|n| verse(n))
+        .rev()
+        .collect::<Vec<String>>()
+        .join("\n")
+        .to_string()
 }
