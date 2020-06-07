6dfc650e7df4c99ea47fe828b4e3308cf6d17a0d
tamagohan <fantastipo23@gmail.com>
2020-06-07 20:03:26 +0900

    Imp pascals triangle

--- rust/pascals-triangle/src/lib.rs
+++ rust/pascals-triangle/src/lib.rs
@@ -1,11 +1,37 @@
-pub struct PascalsTriangle;
+pub struct PascalsTriangle {
+    triangle: Vec<Vec<u32>>,
+}
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!("create Pascal's triangle with {} rows", row_count);
+        let rows = Self::new_imp(row_count);
+        Self { triangle: rows }
+    }
+    fn new_imp(row_count: u32) -> Vec<Vec<u32>> {
+        match row_count {
+            0 => vec![],
+            1 => vec![vec![1]],
+            _ => {
+                let mut triangle = Self::new_imp(row_count - 1);
+                let pre_row = triangle.get((row_count - 2) as usize).unwrap();
+                let row = (0..row_count)
+                    .map(|x| {
+                        let l = if x > 0 {
+                            pre_row.get((x - 1) as usize).unwrap_or(&0)
+                        } else {
+                            &0
+                        };
+                        let r = pre_row.get(x as usize).unwrap_or(&0);
+                        l + r
+                    })
+                    .collect::<Vec<u32>>();
+                triangle.push(row);
+                triangle
+            }
+        }
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        unimplemented!();
+        self.triangle.clone()
     }
 }
