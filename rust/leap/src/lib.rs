d39e51c0a3055b7106391c5ba6d32bdce54aae49
tamagohan <fantastipo23@gmail.com>
2020-07-04 14:55:35 +0900

    [Refactoring] Use match clousure

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,15 +1,8 @@
 pub fn is_leap_year(year: u64) -> bool {
-    if year % 4 == 0 {
-        if year % 100 == 0 {
-            if year % 400 == 0 {
-                true
-            } else {
-                false
-            }
-        } else {
-            true
-        }
-    } else {
-        false
+    match year {
+        year if year % 400 == 0 => true,
+        year if year % 100 == 0 => false,
+        year if year % 4 == 0 => true,
+        _ => false,
     }
 }
