0f338ee3e51ef9dad232742fbac25f70a457aeef
tamagohan <fantastipo23@gmail.com>
2020-07-07 20:16:30 +0900

    Imp raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    match n {
+        n if n % 105 == 0 => "PlingPlangPlong".to_string(),
+        n if n % 35 == 0 => "PlangPlong".to_string(),
+        n if n % 21 == 0 => "PlingPlong".to_string(),
+        n if n % 15 == 0 => "PlingPlang".to_string(),
+        n if n % 7 == 0 => "Plong".to_string(),
+        n if n % 5 == 0 => "Plang".to_string(),
+        n if n % 3 == 0 => "Pling".to_string(),
+        _ => n.to_string(),
+    }
 }
