ce8287f1852e5001f7a898f97c48b0a0b59985a9
tamagohan <fantastipo23@gmail.com>
2020-07-13 19:26:52 +0900

    Imp nth-prime

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,3 +1,19 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    (0..n).fold(2, |acc, _| next_prime(acc))
+}
+
+fn next_prime(min: u32) -> u32 {
+    let mut n = min + 1;
+    while !is_prime_number(n) {
+        n = n + 1;
+    }
+    n
+}
+
+fn is_prime_number(n: u32) -> bool {
+    if n < 3 {
+        true
+    } else {
+        (2..(n / 2 + 1)).all(|num| n % num != 0)
+    }
 }
