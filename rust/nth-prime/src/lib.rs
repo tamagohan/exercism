15777765e17fca3735c72553ec195b387850a200
tamagohan <fantastipo23@gmail.com>
2020-07-15 19:42:37 +0900

    [Refactoring] Use iterator

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,13 +1,8 @@
 pub fn nth(n: u32) -> u32 {
-    (0..n).fold(2, |acc, _| next_prime(acc))
-}
-
-fn next_prime(min: u32) -> u32 {
-    let mut n = min + 1;
-    while !is_prime_number(n) {
-        n = n + 1;
-    }
-    n
+    (2..)
+        .filter(|x| is_prime_number(*x))
+        .nth(n as usize)
+        .unwrap()
 }
 
 fn is_prime_number(n: u32) -> bool {
