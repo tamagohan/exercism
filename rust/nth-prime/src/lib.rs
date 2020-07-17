1c589b4f110697bbdda96561c0a5810d9419ea56
tamagohan <fantastipo23@gmail.com>
2020-07-17 19:30:58 +0900

    [Refactoring] make function name short and simple

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,10 +1,7 @@
 pub fn nth(n: u32) -> u32 {
-    (2..)
-        .filter(|i| is_prime_number(*i))
-        .nth(n as usize)
-        .unwrap()
+    (2..).filter(|i| is_prime(*i)).nth(n as usize).unwrap()
 }
 
-fn is_prime_number(n: u32) -> bool {
+fn is_prime(n: u32) -> bool {
     !(2..(n / 2 + 1)).any(|i| n % i == 0)
 }
