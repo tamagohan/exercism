fd0e999dec379c7ffb1ff1ee96c2d61fb0ceb196
tamagohan <fantastipo23@gmail.com>
2020-07-21 20:53:25 +0900

    Imp proverb

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,20 @@
+use std::iter;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+    match list {
+        [] => String::new(),
+        _ => {
+            let hoge1 = list.iter().zip((list[1..].iter()).chain(iter::once(&"")));
+            return hoge1
+                .map(|(x, y)| {
+                    if y != &"" {
+                        format!("For want of a {} the {} was lost.", x, y)
+                    } else {
+                        format!("And all for the want of a {}.", list[0])
+                    }
+                })
+                .collect::<Vec<_>>()
+                .join("\n");
+        }
+    }
 }
