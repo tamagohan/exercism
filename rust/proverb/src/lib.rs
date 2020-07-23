b3711abd179fce2fd944cd450516e32d581965ee
tamagohan <fantastipo23@gmail.com>
2020-07-23 22:01:28 +0900

    [Refactoring] Rename variable

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -7,9 +7,9 @@ pub fn build_proverb(list: &[&str]) -> String {
             return list
                 .iter()
                 .zip((list[1..].iter()).chain(iter::once(&"")))
-                .map(|(x, y)| {
-                    if y != &"" {
-                        format!("For want of a {} the {} was lost.", x, y)
+                .map(|(w1, w2)| {
+                    if w2 != &"" {
+                        format!("For want of a {} the {} was lost.", w1, w2)
                     } else {
                         format!("And all for the want of a {}.", list[0])
                     }
