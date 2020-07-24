26ad6a7ed3307ae253ce5e176e438270baca23f7
tamagohan <fantastipo23@gmail.com>
2020-07-24 22:52:54 +0900

    [Refactoring] Imp build_proverb function using window function

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,21 +1,16 @@
 use std::iter;
 
 pub fn build_proverb(list: &[&str]) -> String {
-    match list {
-        [] => String::new(),
-        _ => {
-            return list
-                .iter()
-                .zip((list[1..].iter()).chain(iter::once(&"")))
-                .map(|(w1, w2)| {
-                    if w2 != &"" {
-                        format!("For want of a {} the {} was lost.", w1, w2)
-                    } else {
-                        format!("And all for the want of a {}.", list[0])
-                    }
-                })
-                .collect::<Vec<_>>()
-                .join("\n");
-        }
+    if list.is_empty() {
+        return String::new();
     }
+
+    list.windows(2)
+        .map(|w| format!("For want of a {} the {} was lost.", w[0], w[1]))
+        .chain(iter::once(format!(
+            "And all for the want of a {}.",
+            list[0]
+        )))
+        .collect::<Vec<_>>()
+        .join("\n")
 }
