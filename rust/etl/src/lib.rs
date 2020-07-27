49bda3e17fb40f363db12d9dc879687ed53f881c
tamagohan <fantastipo23@gmail.com>
2020-07-27 19:50:21 +0900

    Imp etl

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,5 +1,10 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    unimplemented!("How will you transform the tree {:?}?", h)
+    h.into_iter()
+        .flat_map(|(k, vs)| {
+            vs.into_iter()
+                .map(move |v| (v.to_lowercase().next().unwrap(), *k))
+        })
+        .collect()
 }
