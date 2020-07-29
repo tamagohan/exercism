3de53444da155791917c8ac7c65bb41f29bf5369
tamagohan <fantastipo23@gmail.com>
2020-07-29 20:10:55 +0900

    [Refactoring] Should use iter(), not into_iter()

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,9 +1,9 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    h.into_iter()
+    h.iter()
         .flat_map(|(k, vs)| {
-            vs.into_iter()
+            vs.iter()
                 .map(move |v| (v.to_lowercase().next().unwrap(), *k))
         })
         .collect()
