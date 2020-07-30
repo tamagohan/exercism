c1fe72017fa365f58c3b1b58c38cbc21ee0c6624
tamagohan <fantastipo23@gmail.com>
2020-07-30 18:47:39 +0900

    [Refactoring] patterm match as reference

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -2,9 +2,9 @@ use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
     h.iter()
-        .flat_map(|(k, vs)| {
+        .flat_map(|(&k, vs)| {
             vs.iter()
-                .map(move |v| (v.to_lowercase().next().unwrap(), *k))
+                .map(move |v| (v.to_lowercase().next().unwrap(), k))
         })
         .collect()
 }
