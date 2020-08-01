73d048d0d2c22de111e452160e3fb04881e4b966
tamagohan <fantastipo23@gmail.com>
2020-08-01 18:45:14 +0900

    [Refactoring] should use to_ascii_lowercase(), not to_lowercase()

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -2,9 +2,6 @@ use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
     h.iter()
-        .flat_map(|(&k, vs)| {
-            vs.iter()
-                .map(move |v| (v.to_lowercase().next().unwrap(), k))
-        })
+        .flat_map(|(&k, vs)| vs.iter().map(move |v| (v.to_ascii_lowercase(), k)))
         .collect()
 }
