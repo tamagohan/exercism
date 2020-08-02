8b038616100c779326f4610fa2af52395ae1769b
tamagohan <fantastipo23@gmail.com>
2020-08-02 20:42:10 +0900

    HyperText should be converted to HT

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,3 +1,24 @@
+fn to_abbreviate(word: &str) -> String {
+    if !word.is_empty() && word.chars().all(|c| c.is_uppercase()) {
+        return word
+            .chars()
+            .next()
+            .unwrap()
+            .to_ascii_uppercase()
+            .to_string();
+    }
+    word.chars()
+        .filter(|c| c.is_uppercase())
+        .map(|c| c.to_ascii_uppercase())
+        .collect()
+}
+
 pub fn abbreviate(phrase: &str) -> String {
-    unimplemented!("Given the phrase '{}', return its acronym", phrase);
+    phrase
+        .split(" ")
+        .map(|word| {
+            println!("{:?}", word);
+            to_abbreviate(word)
+        })
+        .collect()
 }
--- rust/acronym/tests/acronym.rs
+++ rust/acronym/tests/acronym.rs
@@ -6,7 +6,6 @@ fn empty() {
 }
 
 #[test]
-#[ignore]
 fn basic() {
     assert_eq!(acronym::abbreviate("Portable Network Graphics"), "PNG");
 }
@@ -32,7 +31,10 @@ fn punctuation() {
 #[test]
 #[ignore]
 fn all_caps_word() {
-    assert_eq!(acronym::abbreviate("GNU Image Manipulation Program"), "GIMP");
+    assert_eq!(
+        acronym::abbreviate("GNU Image Manipulation Program"),
+        "GIMP"
+    );
 }
 
 #[test]
@@ -54,7 +56,9 @@ fn punctuation_without_whitespace() {
 #[ignore]
 fn very_long_abbreviation() {
     assert_eq!(
-        acronym::abbreviate("Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me"),
+        acronym::abbreviate(
+            "Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me"
+        ),
         "ROTFLSHTMDCOALM"
     );
 }
@@ -71,17 +75,11 @@ fn consecutive_delimiters() {
 #[test]
 #[ignore]
 fn apostrophes() {
-    assert_eq!(
-        acronym::abbreviate("Halley's Comet"),
-        "HC"
-    );
+    assert_eq!(acronym::abbreviate("Halley's Comet"), "HC");
 }
 
 #[test]
 #[ignore]
 fn underscore_emphasis() {
-    assert_eq!(
-        acronym::abbreviate("The Road _Not_ Taken"),
-        "TRNT"
-    );
+    assert_eq!(acronym::abbreviate("The Road _Not_ Taken"), "TRNT");
 }
