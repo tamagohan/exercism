357d472e2c81e23c6921e165b4cac8d84c8462c4
tamagohan <fantastipo23@gmail.com>
2020-08-03 18:23:04 +0900

    Imp abbreviate function

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,21 +1,30 @@
 fn to_abbreviate(word: &str) -> String {
-    if !word.is_empty() && word.chars().all(|c| c.is_uppercase()) {
-        return word
-            .chars()
-            .next()
-            .unwrap()
-            .to_ascii_uppercase()
-            .to_string();
+    if word.is_empty() {
+        return word.to_string();
     }
-    word.chars()
-        .filter(|c| c.is_uppercase())
-        .map(|c| c.to_ascii_uppercase())
-        .collect()
+
+    let first_letter = word.chars().next().unwrap();
+    if first_letter.is_lowercase() {
+        return first_letter.to_ascii_uppercase().to_string();
+    } else if word.chars().all(|c| c.is_uppercase()) {
+        return first_letter.to_ascii_uppercase().to_string();
+    } else {
+        word.chars()
+            .filter(|c| c.is_uppercase())
+            .map(|c| c.to_ascii_uppercase())
+            .collect()
+    }
 }
 
 pub fn abbreviate(phrase: &str) -> String {
-    phrase
-        .split(" ")
+    let words = phrase.split(|c| c == ' ' || c == '-').collect::<Vec<_>>();
+    let first_word = words[0];
+    if first_word.ends_with(":") {
+        return first_word.to_string().trim_end_matches(':').to_string();
+    }
+
+    words
+        .iter()
         .map(|word| {
             println!("{:?}", word);
             to_abbreviate(word)
--- rust/acronym/tests/acronym.rs
+++ rust/acronym/tests/acronym.rs
@@ -11,25 +11,21 @@ fn basic() {
 }
 
 #[test]
-#[ignore]
 fn lowercase_words() {
     assert_eq!(acronym::abbreviate("Ruby on Rails"), "ROR");
 }
 
 #[test]
-#[ignore]
 fn camelcase() {
     assert_eq!(acronym::abbreviate("HyperText Markup Language"), "HTML");
 }
 
 #[test]
-#[ignore]
 fn punctuation() {
     assert_eq!(acronym::abbreviate("First In, First Out"), "FIFO");
 }
 
 #[test]
-#[ignore]
 fn all_caps_word() {
     assert_eq!(
         acronym::abbreviate("GNU Image Manipulation Program"),
@@ -38,13 +34,11 @@ fn all_caps_word() {
 }
 
 #[test]
-#[ignore]
 fn all_caps_word_with_punctuation() {
     assert_eq!(acronym::abbreviate("PHP: Hypertext Preprocessor"), "PHP");
 }
 
 #[test]
-#[ignore]
 fn punctuation_without_whitespace() {
     assert_eq!(
         acronym::abbreviate("Complementary metal-oxide semiconductor"),
@@ -53,7 +47,6 @@ fn punctuation_without_whitespace() {
 }
 
 #[test]
-#[ignore]
 fn very_long_abbreviation() {
     assert_eq!(
         acronym::abbreviate(
@@ -64,7 +57,6 @@ fn very_long_abbreviation() {
 }
 
 #[test]
-#[ignore]
 fn consecutive_delimiters() {
     assert_eq!(
         acronym::abbreviate("Something - I made up from thin air"),
@@ -73,13 +65,11 @@ fn consecutive_delimiters() {
 }
 
 #[test]
-#[ignore]
 fn apostrophes() {
     assert_eq!(acronym::abbreviate("Halley's Comet"), "HC");
 }
 
 #[test]
-#[ignore]
 fn underscore_emphasis() {
     assert_eq!(acronym::abbreviate("The Road _Not_ Taken"), "TRNT");
 }
