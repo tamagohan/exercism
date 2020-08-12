82d45b1e869a86f44aff99d11c15c6c5a510438c
tamagohan <fantastipo23@gmail.com>
2020-08-12 23:28:55 +0900

    Imp sieve

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,3 +1,7 @@
+fn is_prime(n: u64) -> bool {
+    !(2..(n / 2 + 1)).any(|i| n % i == 0)
+}
+
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    unimplemented!("Construct a vector of all primes up to {}", upper_bound);
+    (2..(upper_bound + 1)).filter(|i| is_prime(*i)).collect()
 }
