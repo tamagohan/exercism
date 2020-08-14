81866ec300ea4a090775f2171f16c269034a2ead
tamagohan <fantastipo23@gmail.com>
2020-08-14 19:14:29 +0900

    [Refactoring] utilize cache

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,7 +1,16 @@
-fn is_prime(n: u64) -> bool {
-    !(2..(n / 2 + 1)).any(|i| n % i == 0)
-}
-
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    (2..(upper_bound + 1)).filter(|&i| is_prime(i)).collect()
+    let u: usize = upper_bound as usize;
+    let mut primes = vec![false; u + 1];
+    (2..u + 1)
+        .filter_map(|i| {
+            if primes[i] {
+                return None;
+            }
+
+            for m in 1..=(u / i) {
+                primes[m * i] = true
+            }
+            Some(i as u64)
+        })
+        .collect()
 }
