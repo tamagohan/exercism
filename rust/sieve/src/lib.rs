b3d38ccb82cbadb1e17d544b1e15165570431e94
tamagohan <fantastipo23@gmail.com>
2020-08-17 15:41:46 +0900

    [Refactoring] Add linebreak after definding variable

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,6 +1,7 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
     let u: usize = upper_bound as usize;
     let mut primes = vec![false; u + 1];
+
     (2..u + 1)
         .filter_map(|i| {
             if primes[i] {
