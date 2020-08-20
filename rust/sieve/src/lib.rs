17f7a04bd6ccd70fd80593448b43f78450d40876
tamagohan <fantastipo23@gmail.com>
2020-08-20 21:52:53 +0900

    [Refactoring] define range with equal

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,8 +1,8 @@
-pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
+pub fn primes_up_to(upper_bound: usize) -> Vec<u64> {
     let u: usize = upper_bound as usize;
     let mut primes = vec![false; u + 1];
 
-    (2..u + 1)
+    (2..=u)
         .filter_map(|i| {
             if primes[i] {
                 return None;
