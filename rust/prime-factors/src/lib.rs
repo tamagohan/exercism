98426f37a8822a9f2e588fe3336dcab12f7cb2cb
tamagohan <fantastipo23@gmail.com>
2020-08-25 18:59:00 +0900

    Imp by utilizing a recursive function

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,3 +1,20 @@
+use std::vec::Vec;
+
+fn factors_imp(n: u64, l: u64, list: &mut Vec<u64>) {
+    if n % l == 0 {
+        list.push(l);
+        factors_imp(n / l, l, list);
+    } else {
+        if l < n {
+            factors_imp(n, l + 1, list);
+        } else {
+            return;
+        }
+    }
+}
+
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    let mut factors: Vec<u64> = Vec::new();
+    factors_imp(n, 2, &mut factors);
+    factors
 }
