bd0483421c154fe58f69742412f76b0cef69eb8a
tamagohan <fantastipo23@gmail.com>
2020-08-28 21:33:43 +0900

    [Refactoring] Rename function

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,12 +1,12 @@
 use std::vec::Vec;
 
-fn factors_imp(n: u64, l: u64, list: &mut Vec<u64>) {
+fn cal_factors(n: u64, l: u64, list: &mut Vec<u64>) {
     if n % l == 0 {
         list.push(l);
-        factors_imp(n / l, l, list);
+        cal_factors(n / l, l, list);
     } else {
         if l < n {
-            factors_imp(n, l + 1, list);
+            cal_factors(n, l + 1, list);
         } else {
             return;
         }
@@ -15,6 +15,6 @@ fn factors_imp(n: u64, l: u64, list: &mut Vec<u64>) {
 
 pub fn factors(n: u64) -> Vec<u64> {
     let mut factors: Vec<u64> = Vec::new();
-    factors_imp(n, 2, &mut factors);
+    cal_factors(n, 2, &mut factors);
     factors
 }
