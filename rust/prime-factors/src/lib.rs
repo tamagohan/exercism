0e3bad565a0ec93adc5be249d873df0213370401
tamagohan <fantastipo23@gmail.com>
2022-06-11 12:39:59 +0900

    Imp PrimeFactors

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,20 +1,39 @@
 use std::vec::Vec;
 
-fn cal_factors(n: u64, l: u64, list: &mut Vec<u64>) {
-    if n % l == 0 {
-        list.push(l);
-        cal_factors(n / l, l, list);
-    } else {
-        if l < n {
-            cal_factors(n, l + 1, list);
-        } else {
-            return;
+struct Factorization {
+    divided: u64,
+    max_factor: u64,
+}
+
+impl Factorization {
+    fn new(divided: u64) -> Self {
+        Self {
+            divided: divided,
+            max_factor: 2,
+        }
+    }
+}
+
+impl Iterator for Factorization {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.divided == 1 {
+                return None;
+            }
+
+            if self.divided % self.max_factor == 0 {
+                self.divided = self.divided / self.max_factor;
+                return Some(self.max_factor);
+            }
+
+            self.max_factor += 1;
         }
     }
 }
 
 pub fn factors(n: u64) -> Vec<u64> {
-    let mut factors: Vec<u64> = Vec::new();
-    cal_factors(n, 2, &mut factors);
-    factors
+    let factor = Factorization::new(n);
+    factor.collect()
 }
